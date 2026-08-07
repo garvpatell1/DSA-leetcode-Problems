12be1004f21778b6670791a6b7e7bfa9141026f2
Garv Miteshkumar Patel <garvpatell.14@gmail.com>
2026-08-07 16:02:09 +0530

    Time: 5 ms (81.26%), Space: 45.4 MB (98.34%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int rev = 0;
+        while(temp > 0){
+            int rem = temp % 10;
+            rev = ( rev * 10 ) + rem;
+            temp /= 10;
+        }
+        if(rev == x){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
